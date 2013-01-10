f3a0247b4e1034901d3211d042dc04089e04f96b
Maxim Shvecov <Gris87@yandex.ru>
2013-01-10 11:02:24 +0400

    Save/Load name

--- src/com/gris/ege/MainActivity.java
+++ src/com/gris/ege/MainActivity.java
@@ -1,6 +1,7 @@
 package com.gris.ege;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -10,15 +11,17 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity implements OnClickListener
 {
-    private final int CHOOSE_VIEW_TASKS   = 0;
-    private final int CHOOSE_START_TEST   = 1;
-    private final int CHOOSE_VIEW_RESULTS = 2;
+    public static final String PREFS_NAME = "Settings";
 
-    EditText mNameEditText;
+    private static final int CHOOSE_VIEW_TASKS   = 0;
+    private static final int CHOOSE_START_TEST   = 1;
+    private static final int CHOOSE_VIEW_RESULTS = 2;
 
-    Button mViewTasksButton;
-    Button mStartTestButton;
-    Button mViewResultsButton;
+    private EditText mNameEditText;
+
+    private Button mViewTasksButton;
+    private Button mStartTestButton;
+    private Button mViewResultsButton;
 
 	@Override
     protected void onCreate(Bundle savedInstanceState)
@@ -27,25 +30,37 @@ public class MainActivity extends Activity implements OnClickListener
 
 		setContentView(R.layout.activity_main);
 
+		// Get controls
 		mNameEditText = (EditText)findViewById(R.id.nameEditText);
 		mViewTasksButton = (Button)findViewById(R.id.viewTasksButton);
 		mStartTestButton = (Button)findViewById(R.id.startTestButton);
 		mViewResultsButton = (Button)findViewById(R.id.viewResultsButton);
 
+		// Set listeners
 		mViewTasksButton.setOnClickListener(this);
 		mStartTestButton.setOnClickListener(this);
 		mViewResultsButton.setOnClickListener(this);
+
+		// Restore preferences
+	    SharedPreferences aSettings = getSharedPreferences(PREFS_NAME, 0);
+	    String aUserName = aSettings.getString("userName", "");
+
+	    mNameEditText.setText(aUserName);
 	}
 
 	public void makeChoose(int aChoose)
 	{
 	    if (mNameEditText.length()>0)
 	    {
-
+	        // Save preferences
+	        SharedPreferences aSettings = getSharedPreferences(PREFS_NAME, 0);
+	        SharedPreferences.Editor aEditor = aSettings.edit();
+	        aEditor.putString("userName", mNameEditText.getText().toString());
+	        aEditor.commit();
 	    }
 	    else
 	    {
-	        Toast.makeText(this, R.string.name_is_empty, Toast.LENGTH_LONG).show();
+	        Toast.makeText(this, R.string.name_is_empty, Toast.LENGTH_SHORT).show();
 	    }
 	}
 
