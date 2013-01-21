3988d547eb6bd22763ad52315834808fb51f552f
Maxim Shvecov <Gris87@yandex.ru>
2013-01-21 12:55:59 +0400

    answerTextView and correct view visibilities

--- src/com/gris/ege/pager/TaskFragment.java
+++ src/com/gris/ege/pager/TaskFragment.java
@@ -13,6 +13,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
@@ -24,14 +25,16 @@ public class TaskFragment extends Fragment
 
 
 
-    private TextView    mTaskHeaderView;
-    private TextView    mTaskStatusView;
-    private ViewFlipper mTaskViewFlipper;
-    private ProgressBar mDownloadProgressBar;
-    private Button      mRetryButton;
-    private ImageView   mTaskImageView;
-    private EditText    mAnswerEditText;
-    private Button      mAnswerButton;
+    private TextView       mTaskHeaderView;
+    private TextView       mTaskStatusView;
+    private ViewFlipper    mTaskViewFlipper;
+    private ProgressBar    mDownloadProgressBar;
+    private Button         mRetryButton;
+    private ImageView      mTaskImageView;
+    private TextView       mAnswerTextView;
+    private RelativeLayout mBottomLayout;
+    private EditText       mAnswerEditText;
+    private Button         mAnswerButton;
 
     private Task mTask;
     private int  mMode;
@@ -65,26 +68,33 @@ public class TaskFragment extends Fragment
         View aView=aInflater.inflate(R.layout.task_page_item, aContainer, false);
 
         // Get controls
-        mTaskHeaderView      = (TextView)aView.findViewById(R.id.taskHeaderTextView);
-        mTaskStatusView      = (TextView)aView.findViewById(R.id.taskStatusTextView);
-        mTaskViewFlipper     = (ViewFlipper)aView.findViewById(R.id.taskViewFlipper);
-        mDownloadProgressBar = (ProgressBar)aView.findViewById(R.id.downloadProgressBar);
-        mRetryButton         = (Button)aView.findViewById(R.id.retryButton);
-        mTaskImageView       = (ImageView)aView.findViewById(R.id.taskImageView);
-        mAnswerEditText      = (EditText)aView.findViewById(R.id.answerEditText);
-        mAnswerButton        = (Button)  aView.findViewById(R.id.answerButton);
+        mTaskHeaderView      = (TextView)      aView.findViewById(R.id.taskHeaderTextView);
+        mTaskStatusView      = (TextView)      aView.findViewById(R.id.taskStatusTextView);
+        mTaskViewFlipper     = (ViewFlipper)   aView.findViewById(R.id.taskViewFlipper);
+        mDownloadProgressBar = (ProgressBar)   aView.findViewById(R.id.downloadProgressBar);
+        mRetryButton         = (Button)        aView.findViewById(R.id.retryButton);
+        mTaskImageView       = (ImageView)     aView.findViewById(R.id.taskImageView);
+        mAnswerTextView      = (TextView)      aView.findViewById(R.id.answerTextView);
+        mBottomLayout        = (RelativeLayout)aView.findViewById(R.id.bottomLayout);
+        mAnswerEditText      = (EditText)      aView.findViewById(R.id.answerEditText);
+        mAnswerButton        = (Button)        aView.findViewById(R.id.answerButton);
 
         // Initialize controls
         mTaskHeaderView.setText(getString(R.string.task_header, mTask.getCategory(), mTask.getId()+1));
         updateStatus();
 
-        if (mMode==MODE_VIEW_TASK)
+        switch (mMode)
         {
-            mAnswerButton.setVisibility(View.VISIBLE);
-        }
-        else
-        {
-            mAnswerButton.setVisibility(View.GONE);
+            case MODE_VIEW_TASK:
+                mAnswerTextView.setVisibility(View.GONE);
+            break;
+            case MODE_TEST_TASK:
+                mAnswerTextView.setVisibility(View.GONE);
+                mAnswerButton.setVisibility(View.GONE);
+            break;
+            case MODE_VIEW_RESULT:
+                mBottomLayout.setVisibility(View.GONE);
+            break;
         }
 
         return aView;
@@ -92,24 +102,24 @@ public class TaskFragment extends Fragment
 
     public void updateStatus()
     {
-        if (mMode==MODE_VIEW_TASK)
+        if (mMode==MODE_TEST_TASK)
+        {
+            mTaskStatusView.setVisibility(View.GONE);
+        }
+        else
         {
             mTaskStatusView.setVisibility(View.VISIBLE);
 
             if (mTask.isFinished())
             {
-                mTaskStatusView.setText(getString(R.string.finished));
+                mTaskStatusView.setText(getString(mMode==MODE_VIEW_TASK? R.string.finished : R.string.correct));
                 mTaskStatusView.setTextColor(getResources().getColor(R.color.good));
             }
             else
             {
-                mTaskStatusView.setText(getString(R.string.not_finished));
+                mTaskStatusView.setText(getString(mMode==MODE_VIEW_TASK? R.string.not_finished : R.string.not_correct));
                 mTaskStatusView.setTextColor(getResources().getColor(R.color.bad));
             }
-        }
-        else
-        {
-            mTaskStatusView.setVisibility(View.GONE);
         }
     }
 }
